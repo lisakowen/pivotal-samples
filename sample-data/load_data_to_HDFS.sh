d571c6f315d5097520f4eb6d41dfa3cb49710745
Lisa Owen <lowen@pivotal.io>
2016-07-27 16:14:55 -0700

    initial mods to make the load to hdfs script work

--- sample-data/load_data_to_HDFS.sh
+++ sample-data/load_data_to_HDFS.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
+#!/bin/bash 
 
-base_dir="/retail_demo"
+# this script must be run from the sample_data/ directory
 
-# Clean up any previous load
-echo "hadoop fs -rm -r -skipTrash $base_dir"
-hadoop fs -rm -r -skipTrash $base_dir 
+CURDIR=`pwd`
+#echo "CURDIR is $CURDIR"
+HDFS_BASEDIR="/retail_demo"
 
-echo "hadoop fs -mkdir $base_dir"
-hadoop fs -mkdir $base_dir
+# clean up if previous load
+echo "running: sudo -u hdfs hdfs -rm -r -skipTrash $HDFS_BASEDIR"
+sudo -u hdfs hdfs dfs -rm -r -skipTrash $HDFS_BASEDIR
 
-for file in *.tsv.gz
+# create directory on hdfs
+sudo -u hdfs hdfs dfs -mkdir $HDFS_BASEDIR
+
+for file in $CURDIR/*.tsv.gz
 do
-  dir=`echo $file | perl -ne 's/^(.+?)\..+$/$1/;print;'`
-  echo "hadoop fs -mkdir $base_dir/$dir"
-  hadoop fs -mkdir $base_dir/$dir
-  echo "hadoop fs -put $file $base_dir/$dir/"
-  hadoop fs -put $file $base_dir/$dir/
+  #echo "file is $file"
+  basefile=$(basename $file)
+  #echo "basefile is $basefile"
+  dir=`echo $basefile | perl -ne 's/^(.+?)\..+$/$1/;print;'`
+  #echo "dir is $dir"
+  echo "sudo -u hdfs hdfs dfs -mkdir $HDFS_BASEDIR/$dir"
+  sudo -u hdfs hdfs dfs -mkdir $HDFS_BASEDIR/$dir
+  echo "sudo -u hdfs hdfs dfs -put $file $HDFS_BASEDIR/$dir/"
+  sudo -u hdfs hdfs dfs -put $file $HDFS_BASEDIR/$dir/
 done
 
